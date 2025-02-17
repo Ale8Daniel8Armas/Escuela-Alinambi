3e0f5d56914863c54bcb39ccbd68e6448f47d44e
Ale8Daniel8Armas <daarmas10@espe.edu.ec>
2025-02-17 18:04:18 -0500

    remejorando responsividad V2

--- src/components/Navbars/ExamplesNavbar.js
+++ src/components/Navbars/ExamplesNavbar.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classnames from "classnames";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa"; // Iconos para indicar submenús
-
 import {
   Collapse,
   NavbarBrand,
@@ -26,7 +24,7 @@ function ExamplesNavbar() {
   const [ofertaEducativaOpen, setOfertaEducativaOpen] = React.useState(false);
   const [infoOpen, setInfoOpen] = React.useState(false);
   const [isLargeScreen, setIsLargeScreen] = React.useState(
-    window.innerWitdh < 1065
+    window.innerWidth < 1165
   );
 
   const toggleNavbarCollapse = () => {
@@ -46,10 +44,6 @@ function ExamplesNavbar() {
     setInfoOpen(!infoOpen);
   };
 
-  const handleMenuClick = (e) => {
-    e.stopPropagation();
-  };
-
   React.useEffect(() => {
     const updateNavbarColor = () => {
       if (
@@ -66,11 +60,11 @@ function ExamplesNavbar() {
     };
 
     const handleResize = () => {
-      setIsLargeScreen(window.innerWitdh < 1065);
+      setIsLargeScreen(window.innerWidth < 1165);
     };
 
     window.addEventListener("scroll", updateNavbarColor);
-    window.addEventListener("rezise", handleResize);
+    window.addEventListener("resize", handleResize);
 
     return function cleanup() {
       window.removeEventListener("scroll", updateNavbarColor);
@@ -83,7 +77,7 @@ function ExamplesNavbar() {
       <div
         style={{
           width: "0%",
-          height: "14vh",
+          height: "16vh",
           backgroundColor: "#000000",
         }}
       >
@@ -116,7 +110,7 @@ function ExamplesNavbar() {
               })}
               onClick={toggleNavbarCollapse}
               style={{
-                display: isLargeScreen ? "none" : "",
+                display: isLargeScreen ? "block" : "none",
                 border: "none",
                 background: "transparent",
                 padding: "5px",
@@ -131,68 +125,75 @@ function ExamplesNavbar() {
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "flex-end",
+                flexDirection: isLargeScreen ? "column" : "row",
               }}
               navbar
               isOpen={navbarCollapse}
             >
-              <Nav navbar style={{ gap: "20px" }}>
-                <NavItem>
-                  <NavbarBrand
-                    to="/homePage"
-                    target="_blank"
-                    tag={Link}
+              <Nav
+                navbar
+                style={{
+                  gap: "10px",
+                  flexDirection: isLargeScreen ? "column" : "row",
+                  marginLeft: "auto",
+                }}
+              >
+                <NavItem
+                  to="/homePage"
+                  target="_blank"
+                  tag={Link}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "10px",
+                    marginRight: "130px",
+                    marginLeft: "20px",
+                  }}
+                >
+                  <img
+                    src={require("assets/img/Alinambi/LogoAlinambiT.png")}
+                    alt="Logo de Empresa"
+                    style={{
+                      maxHeight: "65px",
+                      width: "auto",
+                      objectFit: "contain",
+                    }}
+                  />
+                  <div
                     style={{
                       display: "flex",
-                      alignItems: "center",
-                      gap: "10px",
-                      marginRight: "40px",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      gap: "2px",
+                      marginLeft: "5px",
                     }}
                   >
-                    <img
-                      src={require("assets/img/Alinambi/LogoAlinambiT.png")}
-                      alt="Logo de Empresa"
+                    <span
                       style={{
-                        maxHeight: "65px",
-                        width: "auto",
-                        objectFit: "contain",
-                      }}
-                    />
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        gap: "2px",
-                        marginLeft: "5px",
+                        fontSize: "20px",
+                        fontWeight: "800",
+                        color:
+                          navbarColor === "navbar-transparent"
+                            ? "#98FF98"
+                            : "#FFFFFF",
+                        lineHeight: "1.1",
+                        fontFamily: "'Montserrat', sans-serif",
                       }}
                     >
-                      <span
-                        style={{
-                          fontSize: "20px",
-                          fontWeight: "500",
-                          color:
-                            navbarColor === "navbar-transparent"
-                              ? "#98FF98"
-                              : "#FFFFFF",
-                          lineHeight: "1.0",
-                          fontFamily: "'Montserrat', sans-serif",
-                        }}
-                      >
-                        Unidad Educativa
-                      </span>
-                      <span
-                        style={{
-                          fontSize: "22px",
-                          fontWeight: "600",
-                          color: "#1A9BD5",
-                          lineHeight: "1.0",
-                          fontFamily: "'Montserrat', sans-serif",
-                        }}
-                      >
-                        Aliñambi
-                      </span>
-                    </div>
-                  </NavbarBrand>
+                      Unidad Educativa
+                    </span>
+                    <span
+                      style={{
+                        fontSize: "22px",
+                        fontWeight: "600",
+                        color: "#1A9BD5",
+                        lineHeight: "1.0",
+                        fontFamily: "'Montserrat', sans-serif",
+                      }}
+                    >
+                      Aliñambi
+                    </span>
+                  </div>
                 </NavItem>
                 <NavItem>
                   <UncontrolledDropdown
@@ -215,7 +216,7 @@ function ExamplesNavbar() {
                         style={{
                           fontSize: "14px",
                           color: "#000000",
-                          marginTop: "30px",
+                          marginTop: "20px",
                           fontFamily: "'Montserrat', sans-serif",
                         }}
                       >
@@ -269,7 +270,6 @@ function ExamplesNavbar() {
                     </DropdownMenu>
                   </UncontrolledDropdown>
                 </NavItem>
-                {/* Los demás NavItems siguen el mismo patrón */}
                 <NavItem>
                   <UncontrolledDropdown
                     nav
@@ -291,7 +291,7 @@ function ExamplesNavbar() {
                         style={{
                           fontSize: "14px",
                           color: "#000000",
-                          marginTop: "30px",
+                          marginTop: "20px",
                           fontFamily: "'Montserrat', sans-serif",
                         }}
                       >
@@ -374,7 +374,7 @@ function ExamplesNavbar() {
                         style={{
                           fontSize: "14px",
                           color: "#000000",
-                          marginTop: "30px",
+                          marginTop: "20px",
                           fontFamily: "'Montserrat', sans-serif",
                         }}
                       >
@@ -385,7 +385,7 @@ function ExamplesNavbar() {
                       aria-labelledby="infoNavbarDropdownMenu"
                       style={{
                         fontFamily: "'Montserrat', sans-serif",
-                        marginBottom: "-260px",
+                        marginBottom: "-40px",
                       }}
                     >
                       <DropdownItem
@@ -446,7 +446,7 @@ function ExamplesNavbar() {
                       style={{
                         fontSize: "14px",
                         color: "#000000",
-                        marginTop: "30px",
+                        marginTop: "20px",
                         fontFamily: "'Montserrat', sans-serif",
                       }}
                     >
@@ -464,7 +464,7 @@ function ExamplesNavbar() {
                       style={{
                         fontSize: "14px",
                         color: "#000000",
-                        marginTop: "30px",
+                        marginTop: "20px",
                         fontFamily: "'Montserrat', sans-serif",
                       }}
                     >
